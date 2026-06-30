423f24cb96404ba080c827e37b6f40b55cd22d8b
hyuntech <timue0829@gmail.com>
2026-07-01 05:15:26 +0900

    transfer emit test

--- test/MyToken.ts
+++ test/MyToken.ts
@@ -49,15 +49,26 @@ describe("My Token", () => {
 
   describe("Transfer", () => {
     it("should have 0.5MT", async () => {
+      const signer0 = signers[0];
       const signer1 = signers[1];
-      await myTokenC.transfer(
-        hre.ethers.parseUnits("0.5", decimals),
-        signer1.address,
-      );
-      console.log(await myTokenC.balanceOf(signer1.address));
+
+      await expect(
+        myTokenC.transfer(
+          hre.ethers.parseUnits("0.5", decimals),
+          signer1.address,
+        ),
+      )
+        .to.emit(myTokenC, "Transfer")
+        .withArgs(
+          signer0.address,
+          signer1.address,
+          hre.ethers.parseUnits("0.5", decimals),
+        );
+
       expect(await myTokenC.balanceOf(signer1.address)).equal(
         hre.ethers.parseUnits("0.5", decimals),
       );
+      expect;
     });
 
     it("should be reverted with insufficient balance error", async () => {
