d7930c16dcf8d1bf87b0735ac74c936f7cd7cb19
hyuntech <timue0829@gmail.com>
2026-07-05 16:53:13 +0900

    add reward logic

--- contracts/TinyBank.sol
+++ contracts/TinyBank.sol
@@ -5,12 +5,19 @@ interface IMyToken {
     function transfer(uint256 amount, address to) external;
 
     function transferFrom(address from, address to, uint256 amount) external;
+
+    function mint(uint256 amount, address owner) external;
 }
 
 contract TinyBank {
-    event Stake(address, uint256);
+    event Stake(address from, uint256 amount);
+    event Withdraw(uint256 amount, address to);
 
     IMyToken public stakingToken;
+
+    mapping(address => uint256) public lastClaimedBlock;
+    uint256 rewardPerBlock = 1 * 10 ** 18;
+
     mapping(address => uint256) public staked;
     uint256 public totalstaked;
 
@@ -18,11 +25,31 @@ contract TinyBank {
         stakingToken = _stakingToken;
     }
 
+    //who when?
+    function distributeReward(address to) internal {
+        uint256 blocks = block.number - lastClaimedBlock[to[i]];
+        uint256 reward = (blocks * rewardPerBlock * staked[to]) / totalstaked;
+        stakingToken.mint(reward, to);
+        lastClaimedBlock[to[i]] = block.number;
+    }
+
     function stake(uint256 _amount) external {
+        destributReward();
         require(_amount >= 0, "cannot stake 0 amount");
         stakingToken.transferFrom(msg.sender, address(this), _amount);
         staked[msg.sender] += _amount;
         totalstaked += _amount;
+
         emit Stake(msg.sender, _amount);
+    }
+
+    function withdraw(uint256 _amount) external {
+        destributReward();
+        require(staked[msg.sender] >= _amount, "insufficient staked token");
+        stakingToken.transfer(_amount, msg.sender);
+        staked[msg.sender] -= _amount;
+        totalstaked -= _amount;
+
+        emit Withdraw(_amount, msg.sender);
     }
 }
