879a9dd2a77951e8e536e51b4de73b0d745dd9b0
hyuntech <timue0829@gmail.com>
2026-07-05 18:04:52 +0900

    add access manager

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,9 +1,11 @@
+// SPDX-License-Identifier: MIT
 // TOken : smart contract based
 // Btc, eth, xrp, kaia : native token
-//SPDX-License-Identifier : MIT
 pragma solidity ^0.8.28;
 
-contract MyToken {
+import "./ManagedAccess.sol";
+
+contract MyToken is ManagedAccess {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed spender, uint256 amount);
 
@@ -20,7 +22,7 @@ contract MyToken {
         string memory _symbol,
         uint8 _decimals,
         uint256 _amount
-    ) {
+    ) ManagedAccess(msg.sender, msg.sender) {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
@@ -42,18 +44,18 @@ contract MyToken {
         emit Transfer(from, to, amount);
     }
 
-    function mint(uint256 amount, address owner) external {
-        _mint(amount, owner);
+    function mint(uint256 amount, address to) external onlyManager {
+        _mint(amount, to);
     }
 
     //transaction
     // from, to, data, value, gas, ..
 
-    function _mint(uint256 amount, address owner) internal {
+    function _mint(uint256 amount, address to) internal {
         totalSupply += amount;
-        balanceOf[owner] += amount;
+        balanceOf[to] += amount;
 
-        emit Transfer(address(0), owner, amount);
+        emit Transfer(address(0), to, amount);
     }
 
     function transfer(uint256 amount, address to) external {
